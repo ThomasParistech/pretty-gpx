0d1062b0f7a4e3bae58ba78026c6014368498984
Thomas Rouch <thom01.rouch@gmail.com>
2024-09-13 08:35:29 +0200

    :sparkles: add github action with linter and type checker

--- scripts/run_linter.sh
+++ scripts/run_linter.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 ruff check --config .vscode/ruff.toml .
+
+echo "Linting passed successfully!"
--- scripts/run_mypy.sh
+++ scripts/run_mypy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 mypy . --config-file .vscode/.mypy.ini
+
+echo "Type-Checking passed successfully!"
