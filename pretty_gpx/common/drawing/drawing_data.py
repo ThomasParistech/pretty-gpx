44ccfb8b48d9872f4ba774edf1e073228f5d57ff
Thomas Rouch <thom01.rouch@gmail.com>
2024-09-19 14:56:43 +0200

    :zap: remove slow asdict in DrawingData

--- pretty_gpx/common/drawing/drawing_data.py
+++ pretty_gpx/common/drawing/drawing_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Drawing Data."""
-from dataclasses import asdict
 from dataclasses import dataclass
+from dataclasses import fields
 from typing import Literal
 
 from matplotlib.axes import Axes
@@ -16,6 +16,13 @@ class BaseDrawingData:
     def plot(self, ax: Axes, color: str) -> None:
         """Plot the annotation."""
         raise NotImplementedError("Plot method must be implemented in child classes")
+
+    def kwargs(self, skip_xy: bool = False) -> dict:
+        """Return the dataclass fields as a dictionary."""
+        # Don't use asdict(self) as it will return a deepcopy of all non-dataclass fields, which is pretty slow
+        return {f.name: getattr(self, f.name)
+                for f in fields(self)
+                if not (skip_xy and f.name in ('x', 'y'))}
 
 
 @dataclass(kw_only=True)
@@ -32,8 +39,7 @@ class TextData(BaseDrawingData):
 
     def plot(self, ax: Axes, color: str) -> None:
         """Plot the annotation."""
-        kwargs = asdict(self)
-        ax.text(**kwargs, c=color)
+        ax.text(**self.kwargs(), c=color)
 
 
 @dataclass(kw_only=True)
@@ -47,8 +53,7 @@ class PlotData(BaseDrawingData):
 
     def plot(self, ax: Axes, color: str) -> None:
         """Plot the annotation."""
-        kwargs = asdict(self)
-        ax.plot(kwargs.pop('x'), kwargs.pop('y'), **kwargs, c=color)
+        ax.plot(self.x, self.y, **self.kwargs(skip_xy=True), c=color)
 
 
 @dataclass(kw_only=True)
@@ -62,8 +67,7 @@ class ScatterData(BaseDrawingData):
 
     def plot(self, ax: Axes, color: str) -> None:
         """Plot the annotation."""
-        kwargs = asdict(self)
-        ax.plot(kwargs.pop('x'), kwargs.pop('y'), **kwargs,
+        ax.plot(self.x, self.y, **self.kwargs(skip_xy=True),
                 linestyle='', clip_on=False,  # Allow start/end markers to be drawn outside the plot area
                 c=color)
 
@@ -76,5 +80,5 @@ class PolyFillData(BaseDrawingData):
 
     def plot(self, ax: Axes, color: str) -> None:
         """Plot the annotation."""
-        kwargs = asdict(self)
-        ax.fill(kwargs.pop('x'), kwargs.pop('y'), **kwargs, c=color)
+        ax.fill(self.x, self.y, **self.kwargs(skip_xy=True), c=color)
+
