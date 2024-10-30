7813847024420d6c7bc7e7ab10cac142fddac197
Thomas Rouch <thom01.rouch@gmail.com>
2024-10-30 23:38:55 +0100

    :sparkles: add fly_io_setup

--- pretty_gpx/main.py
+++ pretty_gpx/main.py
@@ -5,8 +5,11 @@ from nicegui import app
 from nicegui import ui
 
 from pretty_gpx.common.utils.profile import Profiling
+from pretty_gpx.fly.fly_io_setup import fly_io_setup
 from pretty_gpx.ui.pages.page.header import create_rendering_pages
 from pretty_gpx.ui.pages.page.homepage import create_homepage
+
+fly_io_setup()
 
 create_homepage()
 create_rendering_pages()
