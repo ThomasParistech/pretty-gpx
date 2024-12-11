f9753679228c19b89dbd33156fc06f7a70f5175d
Jules Lecoustre <38831521+JulesL2@users.noreply.github.com>
2024-12-11 08:44:38 +0100

    feat: add bridges and a new Overpass along way feature tracking (#77)
    Co-authored-by: Thomas Rouch <thom01.rouch@gmail.com>

--- pretty_gpx/common/gpx/gpx_data_cache_handler.py
+++ pretty_gpx/common/gpx/gpx_data_cache_handler.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from pathvalidate import sanitize_filename
 
 from pretty_gpx.common.gpx.gpx_bounds import GpxBounds
+from pretty_gpx.common.gpx.gpx_track import GpxTrack
 from pretty_gpx.common.utils.paths import CACHE_DIR
 
 
@@ -19,14 +20,18 @@ class GpxDataCacheHandler:
     def __post_init__(self) -> None:
         assert self.extension.startswith('.')
 
-    def get_path(self, bounds: GpxBounds) -> str:
+    def get_path(self, ref: GpxTrack | GpxBounds) -> str:
         """Get path to the corresponding cache file."""
-        bounds_str = f"{bounds.lon_min:.4f},{bounds.lon_max:.4f},{bounds.lat_min:.4f},{bounds.lat_max:.4f}"
-        bounds_hash = hashlib.sha256(bounds_str.encode('utf-8')).hexdigest()
+        if isinstance(ref, GpxBounds):
+            ref_str = f"{ref.lon_min:.4f},{ref.lon_max:.4f},{ref.lat_min:.4f},{ref.lat_max:.4f}"
+        elif isinstance(ref, GpxTrack):
+            ref_str = ref.get_overpass_lonlat_str()
+        else:
+            raise ValueError(f"Unsupported type {type(ref)}")
 
+        hash_str = hashlib.sha256(ref_str.encode('utf-8')).hexdigest()
         parent_folder = os.path.join(CACHE_DIR, sanitize_filename(self.name.lower()))
-        basename = f"{bounds_hash}{self.extension}"
+        basename = f"{hash_str}{self.extension}"
 
         os.makedirs(parent_folder, exist_ok=True)
-
         return os.path.join(parent_folder, basename)
--- pretty_gpx/rendering_modes/city/data/bridges.py
+++ pretty_gpx/rendering_modes/city/data/bridges.py
@@ -3,11 +3,11 @@
 import os
 from dataclasses import dataclass
 
+from pretty_gpx.common.data.overpass_processing import process_around_ways_and_relations
 from pretty_gpx.common.data.overpass_request import OverpassQuery
-from pretty_gpx.common.gpx.gpx_bounds import GpxBounds
 from pretty_gpx.common.gpx.gpx_data_cache_handler import GpxDataCacheHandler
-from pretty_gpx.common.gpx.gpx_distance import ListLonLat
 from pretty_gpx.common.gpx.gpx_track import GpxTrack
+from pretty_gpx.common.utils.logger import logger
 from pretty_gpx.common.utils.pickle_io import read_pickle
 from pretty_gpx.common.utils.pickle_io import write_pickle
 from pretty_gpx.common.utils.profile import profile
@@ -27,25 +27,23 @@ class CityBridge:
 
 
 @profile
-def prepare_download_city_bridges(query: OverpassQuery, bounds: GpxBounds) -> None:
+def prepare_download_city_bridges(query: OverpassQuery, track: GpxTrack) -> None:
     """Add the queries for city bridges inside the global OverpassQuery."""
-    cache_pkl = BRIDGES_CACHE.get_path(bounds)
+    cache_pkl = BRIDGES_CACHE.get_path(track)
 
     if os.path.isfile(cache_pkl):
         query.add_cached_result(BRIDGES_CACHE.name, cache_file=cache_pkl)
         return
 
-    query.add_overpass_query(array_name=BRIDGES_ARRAY_NAME,
-                             query_elements=['wr["name"]["wikidata"]["man_made"="bridge"]'],
-                             include_tags=True,
-                             return_center_only=True,
-                             bounds=bounds,
-                             add_relative_margin=0.05)
+    query.add_around_ways_overpass_query(array_name=BRIDGES_ARRAY_NAME,
+                                         query_elements=['wr["name"]["wikidata"]["man_made"="bridge"]'],
+                                         gpx_track=track,
+                                         radius_m=40)
 
 
 @profile
 def process_city_bridges(query: OverpassQuery,
-                         bounds: GpxBounds) -> tuple[ListLonLat, list[str]]:
+                         track: GpxTrack) -> list[CityBridge]:
     """Process the overpass API result to get the bridges of a city."""
     if query.is_cached(BRIDGES_CACHE.name):
         cache_file = query.get_cache_file(BRIDGES_CACHE.name)
@@ -53,26 +51,11 @@ def process_city_bridges(query: OverpassQuery,
 
     with Profiling.Scope("Process Bridges"):
         res = query.get_query_result(BRIDGES_ARRAY_NAME)
-        list_wr = res.ways + res.relations
-        bridges_lon_lat = [(wr.center_lon, wr.center_lat) for wr in list_wr]
-        bridges_names = [wr.tags.get("name") for wr in list_wr]
-        named_bridges = (bridges_lon_lat, bridges_names)
+        bridges = process_around_ways_and_relations(api_result=res)
+        bridges_l = [CityBridge(name, lat, lon) for name, (lon, lat) in bridges.items()]
 
-    cache_pkl = BRIDGES_CACHE.get_path(bounds)
-    write_pickle(cache_pkl, named_bridges)
+    logger.info(f"Found {len(bridges_l)} bridge(s)")
+    cache_pkl = BRIDGES_CACHE.get_path(track)
+    write_pickle(cache_pkl, bridges_l)
     query.add_cached_result(BRIDGES_CACHE.name, cache_file=cache_pkl)
-    return named_bridges
-
-
-def get_gpx_track_bridges(bridges_lon_lat: ListLonLat,
-                          bridges_name: list[str],
-                          gpx: GpxTrack) -> list[CityBridge]:
-    """Filter the city bridge that are close to the gpx track."""
-    if len(bridges_lon_lat) == 0:
-        return []
-
-    distances = gpx.get_distances_m(bridges_lon_lat)
-
-    return [CityBridge(name=name, lon=lon, lat=lat)
-            for (lon, lat), name, dist in zip(bridges_lon_lat, bridges_name, distances)
-            if dist < 25]
+    return bridges_l
